1c7c517b245de57ee1748e20a6b19c74cda57dd7
Esteban <funcesteban@gmail.com>
2022-05-29 19:32:47 -0600

    add model

---
+++ TripHelper/TripHelper/model/Places.swift
@@ -0,0 +1,30 @@
+//
+//  Places.swift
+//  TripHelper
+//
+//  Created by Esteban Hernandez on 29/05/22.
+//
+
+import Foundation
+
+struct PlaceElements: Codable {
+    var places: [ PlaceElement ]
+}
+struct PlaceElement: Codable, Hashable {
+    var id: String
+    var title: String
+    var description: String
+    var tags: [String]
+    var cost: [MoneyItem]
+    var accesibility: [String]
+    var dateAdd: Date
+    var photos: [String]
+    var location: String
+    
+}
+struct MoneyItem: Codable, Hashable {
+    var item: String
+    var quantity: Double
+    var aprox: Bool
+    var badge: String
+}
