f6cd0c7ed5a21ad8c0f030cdc12d7a1e76f9a208
Esteban <funcesteban@gmail.com>
2022-05-29 18:40:06 -0600

    template project app

---
+++ TripHelper/TripHelper/TripHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripHelperApp.swift
+//  TripHelper
+//
+//  Created by Esteban Hernandez on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TripHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
